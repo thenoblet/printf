1b2bc5a197a491cd6c381fd7859a89c60cab4130
noblet <appiahpatricknoblet@gmail.com>
2023-11-10 08:00:56 +0000

    added primary files

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - prints a formatted string to stdout
+ * @format: the formatted string
+ *
+ * Return: the number of characters printed to stdout,
+ * else -1 if @format is NULL
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
