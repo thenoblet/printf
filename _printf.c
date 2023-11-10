2c747af9289479f9a925e1c0689fe3de1e89059e
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-10 17:01:12 +0000

    more comments to explain but will be taken out once code is finalized

--- _printf.c
+++ _printf.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -10,5 +13,44 @@
 
 int _printf(const char *format, ...)
 {
+	int count = 0;/*to keep track of the number of characters printed*/
+	char c, *str;/*c to store characters and str to store strings*/
+	va_list args;
 
+	if (!format)/*if string is NULL, indicate error*/
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)/*iterate over each char until null character*/
+	{
+		/*check if char is '%' and is next to either 'c', 's' or '%'*/
+		if (*format == '%' && (*(format + 1) == 'c' ||
+		*(format + 1) == 's' || *(format + 1) == '%'))
+		{
+			/*check for specifier following '%'*/
+			switch (*(format + 1))
+			{
+				case 'c':/*if 'c', retrieve an int and write*/
+					c = va_arg(args, int);
+					count += write(1, &c, 1);
+					break;
+				case 's':/*if char*, write if not NULL*/
+					str = va_arg(args, char *);
+					if (str)
+						count += write(1, str, strlen(str));
+					break;
+				default:
+					count += write(1, format, 1);
+			}
+			format += 2;
+		}
+		else/*write to stdout and increment both count and format*/
+		{
+			count += write(1, format, 1);
+			format++;
+		}
+	}
+	va_end(args);/*end variable argument*/
+	return (count);/*return the total count*/
 }
--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
 
 /* _printf function prototype */
 int _printf(const char *format, ...);
 
 /* custom memory deallocation */
 void _free(void **ptr);
-#define safefree(p) safefree((void**)&(p))
+#define safefree(p) safefree((void **)&(p))
 
 #endif /* MAIN_H */
