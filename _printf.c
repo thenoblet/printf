f8e054634f5d8874366cc5c4f9f698a855d7fc48
Lawson Pascal Israel <isreallawson@outlook.com>
2023-11-10 19:57:16 +0000

    fixed %i issue and renamed file

--- _printf.c
+++ _printf.c
@@ -4,53 +4,113 @@
 #include "main.h"
 
 /**
+ * _write_char - writes a character to stdout
+ * @c: the character to write
+ *
+ * Return: the number of characters written
+ */
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _write_str - writes a string to stdout
+ * @str: the string to write
+ *
+ * Return: the number of characters written
+ */
+int _write_str(const char *str)
+{
+	if (str)
+		return (write(1, str, strlen(str)));
+	return (0);
+}
+
+/**
+ * _write_int - writes an integer to stdout
+ * @n: the integer to write
+ *
+ * Return: the number of character written
+ */
+int _write_int(int n)
+{
+	char buffer[12];
+	int lenght = snprintf(buffer, sizeof(buffer), "%d", n);
+
+	if (lenght < 0)
+	{
+		perror("snprintf");
+		return (0);
+	}
+
+	return (write(1, buffer, lenght));
+}
+
+/**
+ * process_format - processes the format string and writes to stdout
+ * @format: the format string
+ * @args: the variable arguments list
+ *
+ * Return: the number of characters printed to stdout
+ */
+int process_format(const char *format, va_list args)
+{
+	int count = 0;
+	char c, *str;
+
+	while (*format)
+	{
+		if (*format == '%' && (*(format + 1) == 'c' ||
+		*(format + 1) == 's' || *(format + 1) == '%' ||
+		*(format + 1) == 'd' || *(format + 1) == 'i'))
+		{
+			switch (*(format + 1))
+			{
+				case 'c':
+					c = va_arg(args, int);
+					count += _write_char(c);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					count += _write_str(str);
+					break;
+				case 'd':
+				case 'i':
+					count += _write_int(va_arg(args, int));
+					break;
+				default:
+					count += _write_char(*format);
+			}
+			format += 2;
+		}
+		else
+		{
+			count += _write_char(*format);
+			format++;
+		}
+	}
+	return (count);
+}
+
+/**
  * _printf - prints a formatted string to stdout
  * @format: the formatted string
  *
  * Return: the number of characters printed to stdout,
  * else -1 if @format is NULL
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;/*to keep track of the number of characters printed*/
-	char c, *str;/*c to store characters and str to store strings*/
+	int count = 0;
 	va_list args;
 
-	if (!format)/*if string is NULL, indicate error*/
+	if (!format)
 		return (-1);
 
 	va_start(args, format);
+	count = process_format(format, args);
+	va_end(args);
 
-	while (*format)/*iterate over each char until null character*/
-	{
-		/*check if char is '%' and is next to either 'c', 's' or '%'*/
-		if (*format == '%' && (*(format + 1) == 'c' ||
-		*(format + 1) == 's' || *(format + 1) == '%'))
-		{
-			/*check for specifier following '%'*/
-			switch (*(format + 1))
-			{
-				case 'c':/*if 'c', retrieve an int and write*/
-					c = va_arg(args, int);
-					count += write(1, &c, 1);
-					break;
-				case 's':/*if char*, write if not NULL*/
-					str = va_arg(args, char *);
-					if (str)
-						count += write(1, str, strlen(str));
-					break;
-				default:
-					count += write(1, format, 1);
-			}
-			format += 2;
-		}
-		else/*write to stdout and increment both count and format*/
-		{
-			count += write(1, format, 1);
-			format++;
-		}
-	}
-	va_end(args);/*end variable argument*/
-	return (count);/*return the total count*/
+	return (count);
 }
