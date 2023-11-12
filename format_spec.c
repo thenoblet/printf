fcda724d684f2b717f21c7aea63b33800cd4d877
noblet <appiahpatricknoblet@gmail.com>
2023-11-12 15:25:45 +0000

    added comments and TODOs

--- format_spec.c
+++ format_spec.c
@@ -21,7 +21,7 @@ format_spec *format_spec_array(void)
 	format_spec *format_array;
 
 	/* array of format specifiers and their handlers funcs. */
-	static format_spec specifiers[] = {
+	format_spec specifiers[] = {
 		{'s', handle_str}
 
 TODO: Add more specifiers and their handlers as needed
