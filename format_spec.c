a25880ccd09872ec184b979cb47788c426d9a4d5
noblet <appiahpatricknoblet@gmail.com>
2023-11-15 02:24:44 +0000

    APN: format spec

--- format_spec.c
+++ format_spec.c
@@ -27,7 +27,8 @@ format_spec *format_spec_array(void)
 		{'d', handle_int},
 		{'i', handle_int},
 		{'c', handle_char},
-		{'b', handle_binary}
+		{'b', handle_binary},
+		{'S', handle_custom_string}
 		/* TODO: add more specifiers and their handler funcs */
 		/* {'\0', NULL} */
 
